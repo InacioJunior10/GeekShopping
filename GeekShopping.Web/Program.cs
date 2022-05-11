e78bb665484ce1a039dc7d887a44e2a92efa4dfb
InacioJunior10 <inaciojunior10@hotmail.com>
2022-05-10 23:54:04 -0300

    Configurando a comunicação com o ProductAPI

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,4 +1,11 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IServices;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient<IProductService, ProductService>(
+        c => c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"])
+    );
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
