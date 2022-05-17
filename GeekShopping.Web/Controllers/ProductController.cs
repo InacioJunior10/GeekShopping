b2ee6ad8f3d833a60ef1de2073517272ac982c6e
InacioJunior10 <inaciojunior10@hotmail.com>
2022-05-17 00:32:09 -0300

    ProductUpdate e ProductDelete implementados.

--- GeekShopping.Web/Controllers/ProductController.cs
+++ GeekShopping.Web/Controllers/ProductController.cs
@@ -20,7 +20,7 @@ namespace GeekShopping.Web.Controllers
         }
 
         public async Task<IActionResult> ProductCreate()
-        {            
+        {
             return View();
         }
 
@@ -33,6 +33,45 @@ namespace GeekShopping.Web.Controllers
                 if (response != null)
                     return RedirectToAction(nameof(ProductIndex));
             }
+
+            return View(productModel);
+        }
+
+        public async Task<IActionResult> ProductUpdate(int id)
+        {
+            var product = await _productService.FindProductById(id);
+            if (product != null)
+                return View(product);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductUpdate(ProductModel productModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProduct(productModel);
+                if (response != null)
+                    return RedirectToAction(nameof(ProductIndex));
+            }
+
+            return View(productModel);
+        }
+
+        public async Task<IActionResult> ProductDelete(int id)
+        {
+            var product = await _productService.FindProductById(id);
+            if (product != null)
+                return View(product);
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductModel productModel)
+        {
+            var response = await _productService.DeleteProductById(productModel.Id);
+            if (response)
+                return RedirectToAction(nameof(ProductIndex));
 
             return View(productModel);
         }
