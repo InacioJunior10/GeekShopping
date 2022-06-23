90ed589fd7ebac22916a1936d4bd7229ab0f4d25
InacioJunior10 <inaciojunior10@hotmail.com>
2022-06-23 01:32:22 -0300

    Tela de Checkout implementada

--- GeekShopping.Web/Controllers/CartController.cs
+++ GeekShopping.Web/Controllers/CartController.cs
@@ -10,14 +10,17 @@ namespace GeekShopping.Web.Controllers
     {
         private readonly IProductService _productService;
         private readonly ICartService _cartService;
+        private readonly ICouponService _couponService;
 
         public CartController(
                 IProductService productService, 
-                ICartService cartService
+                ICartService cartService,
+                ICouponService couponService
             )
         {
             _productService = productService;
             _cartService = cartService;
+            _couponService = couponService;
         }
 
         [Authorize]
@@ -72,6 +75,12 @@ namespace GeekShopping.Web.Controllers
             return View();
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Checkout()
+        {
+            return View(await FindUserCart());
+        }
+
         private async Task<CartViewModel> FindUserCart()
         {
             var token = await HttpContext.GetTokenAsync("access_token");
@@ -80,10 +89,19 @@ namespace GeekShopping.Web.Controllers
             var response = await _cartService.FindCartByUserId(userId, token);
             if (response?.CartHeader != null)
             {
+                if (!string.IsNullOrEmpty(response.CartHeader.CouponCode))
+                {
+                    var coupon = await _couponService.GetCoupon(response.CartHeader.CouponCode, token);
+                    if (coupon?.CouponCode != null)                    
+                        response.CartHeader.DiscountAmount = coupon.DiscountAmount;                    
+                }
+
                 foreach (var detail in response.CartDetails)
                 {
                     response.CartHeader.PurchaseAmount += (detail.Product.Price * detail.Count);
                 }
+
+                response.CartHeader.PurchaseAmount -= response.CartHeader.DiscountAmount;
             }
 
             return response;
--- GeekShopping.Web/Services/IServices/ICouponService.cs
+++ GeekShopping.Web/Services/IServices/ICouponService.cs
@@ -1,6 +1,9 @@
-﻿namespace GeekShopping.Web.Services.IServices
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.IServices
 {
     public interface ICouponService
     {
+        Task<CouponViewModel> GetCoupon(string code, string token);
     }
 }
